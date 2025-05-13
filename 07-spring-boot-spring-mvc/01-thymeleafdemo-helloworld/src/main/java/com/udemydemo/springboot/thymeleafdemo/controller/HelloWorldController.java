d545eb22f1c1686d35b4b58011d8ace0ac687f21
rafaelbarbarroxa <rafaelbarbarroxa@gmail.com>
2025-05-13 18:33:02 +0100

    Usage of @RequestParam

--- 07-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/udemydemo/springboot/thymeleafdemo/controller/HelloWorldController.java
+++ 07-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/udemydemo/springboot/thymeleafdemo/controller/HelloWorldController.java
@@ -3,6 +3,7 @@ package com.udemydemo.springboot.thymeleafdemo.controller;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -10,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class HelloWorldController {
 
     //method to show initial form
-    @RequestMapping("/showForm")
+    @GetMapping("/showForm")
     public String showForm(){
         return "helloworld-form";
     }
