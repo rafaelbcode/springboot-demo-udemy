04eda8202cb2f508b38a01fcf86f4dfe4f42277f
rafaelbarbarroxa <rafaelbarbarroxa@gmail.com>
2025-05-13 18:50:49 +0100

    process form is now a POST method

--- 07-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/udemydemo/springboot/thymeleafdemo/controller/HelloWorldController.java
+++ 07-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/udemydemo/springboot/thymeleafdemo/controller/HelloWorldController.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -42,7 +43,7 @@ public class HelloWorldController {
     }
 
 
-    @RequestMapping("/processFormVersionThree")
+    @PostMapping("/processFormVersionThree")
     public String processFormVersionThree(@RequestParam("studentName") String theName,
                                           Model model){
 
