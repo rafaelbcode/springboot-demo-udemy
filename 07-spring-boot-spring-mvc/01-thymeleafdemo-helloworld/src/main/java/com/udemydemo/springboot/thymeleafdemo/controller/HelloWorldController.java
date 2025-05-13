acf01d28465abacb3fddb3684ba59b0354e0cfc6
rafaelbarbarroxa <rafaelbarbarroxa@gmail.com>
2025-05-13 17:32:20 +0100

    Thymeleaf RequestParam

--- 07-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/udemydemo/springboot/thymeleafdemo/controller/HelloWorldController.java
+++ 07-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/udemydemo/springboot/thymeleafdemo/controller/HelloWorldController.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
@@ -32,6 +33,23 @@ public class HelloWorldController {
 
         //create the message
         String result = "Hello! " + theName;
+
+        //add message to the model
+        model.addAttribute("message",result);
+
+        return "helloworld";
+    }
+
+
+    @RequestMapping("/processFormVersionThree")
+    public String processFormVersionThree(@RequestParam("studentName") String theName,
+                                          Model model){
+
+        //convert the data to all caps
+        theName = theName.toUpperCase();
+
+        //create the message
+        String result = "Greetings from v3! " + theName;
 
         //add message to the model
         model.addAttribute("message",result);
